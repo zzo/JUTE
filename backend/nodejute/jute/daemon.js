577749db52865866bc506a4b5445319e6aa5cfd2
Mark Trostler <mark@zzo.com>
2012-06-09 16:07:59 -0700

    use daemonize

--- backend/nodejute/jute/daemon.js
+++ backend/nodejute/jute/daemon.js
@@ -3,72 +3,46 @@
 var opt     = require( "optimist"),
     fs      = require('fs'),
     args    = opt
-        .usage('Usage: $0 [ --start ] [ --stop ]')
+        .usage('Usage: $0 [ --start ] [ --stop ] [ --restart ] [ --kill ]')
         .alias('s', 'start')
         .alias('p', 'stop')
+        .alias('r', 'restart')
+        .alias('k', 'kill')
         .describe('start', 'Start JUTE')
         .describe('stop', 'Stop JUTE')
         .argv,
     pidfile = process.env['npm_package_config_pidfile'] || '/tmp/jute.pid'
+    user    = process.env['npm_package_config_asuser'] || ''
     ;
 
-function start() {
-    var spawn = require('child_process').spawn, jute;
-
-    try {
-        var pid = fs.readFileSync(pidfile);
-        if (pid) {
-            // is pid actually running?
-            try {
-                process.kill(pid, 0);
-                console.error('JUTE currently running: ' + pid + ".  Maybe you want to 'stop' it first?");
-                process.exit(0);
-            } catch(e) {
-                // process does not exist!
-                try {
-                    fs.unlinkSync(pidfile);
-                } catch(err) {}
-            }
-        }
-    } catch(e) {
-        // good to go
-    }
-
-    try {
-        jute = spawn('./jute_backend.js', [], { setsid: false, cwd: process.cwd() });
-    } catch(e) {
-        console.error("Error spawing JUTE: " + e);
-        process.exit(1);
-    }
-
-    jute.on('exit', function() {  try { fs.unlinkSync(pidfile); } catch(e) {} });
-
-    try {
-        fs.writeFileSync(pidfile, "" + jute.pid);
-    } catch(e) {
-        console.error("Error writing pidFile: " + pidfile);
-    }
-}
-
-function stop() {
-    try {
-        var pid = fs.readFileSync(pidfile);
-        process.kill(pid, 'SIGTERM');
-    } catch(e) {
-        // not running?
-    }
-
-    try {
-        fs.unlinkSync(pidfile);
-    } catch(e) {}
-}
+var daemon = require("daemonize").setup({
+    main:      "../jute_backend.js"
+    , name:    "jute"
+    , pidfile: pidfile
+    , user: user
+});
 
 if (args.start) {
-    start();
+    daemon.start().once("started", function() {
+        process.exit();
+    });
 } else if (args.stop) {
-    stop();
+    daemon.stop();
+} else if (args.restart) {
+    if (daemon.status()) {
+        daemon.stop().once("stopped", function() {
+            daemon.start().once("started", function() {
+                process.exit();
+            });
+        });
+    } else {
+        daemon.start().once("started", function() {
+            process.exit();
+        });
+    }
+} else if (args.kill) {
+    daemon.kill();
 } else {
     console.error("You must specify 'start' or 'stop'!");
 }
 
-//process.exit(0);
